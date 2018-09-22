b71acb1f928d5d4152f3f390888396058ab739a3
Shubham <princeshubham22699@gmail.com>
2018-09-22 11:03:50 +0530

    Node MongoDB OPerations Using File module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/conFusion';
 
 MongoClient.connect(url , (err , db) => {
@@ -11,6 +12,37 @@ MongoClient.connect(url , (err , db) => {
 
     const collection = db.collection('dishes');
     
+    //   *** Method-2 (Using a File Based Module) ***
+    dboper.insertDocument(db , { "name": "Capacino", "description": "Test"} , 
+        "dishes" , (result) => {
+            console.log('Inserted Documnet:\n' , result.ops);
+
+            dboper.findDocuments(db , "dishes" , (docs) => {
+                console.log('Found Documents:\n', docs);
+
+                dboper.updateDocument(db , {"name": "Capacino"} ,
+                    { "description": "Updated Test" } , "dishes" ,
+                    (result) => {
+                        console.log('Updated Document:\n' , result.result);
+
+                        dboper.findDocuments(db , "dishes" , (docs) => {
+                            console.log('Found Udated Documents:\n', docs);
+                        
+                            db.dropCollection("dishes" , (result) => {
+                                console.log("Dropped Collection: " , result);
+
+                                db.close();
+                            });
+                        });    
+                    });
+            });
+        });
+
+
+
+
+    /*    *** Method - 1 (Normal    Without Creating a File based Module) ***
+
     collection.insertOne({"name": "Uthapizza", "description": "test"} ,
         (err , result) => {
             assert.equal(err , null);
@@ -31,4 +63,5 @@ MongoClient.connect(url , (err , db) => {
                 })
             })
         });
+    */    
 });
