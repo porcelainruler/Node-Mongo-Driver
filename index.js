d9f0a9b3215ec51bc71add029cd3a922d5177489
Shubham <princeshubham22699@gmail.com>
2018-09-22 11:46:01 +0530

    Added Callback hell Solution Using Promises - (.then() and .catch())

--- index.js
+++ index.js
@@ -4,15 +4,54 @@ const assert = require('assert');
 const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url , (err , db) => {
-
-    assert.equal(err , null);
+MongoClient.connect(url).then((db) => {
 
     console.log('Connected correctly to the Server');
+  // *** Method-3 (Using a File Based Module - Included Promises to Solve Callback Hell) ***
+    dboper.insertDocument(db , { "name": "Capacino", "description": "Test"} , 
+        "dishes")
+        .then((result) => {
+            console.log('Inserted Documnet:\n' , result.ops);
 
-    const collection = db.collection('dishes');
+            return dboper.findDocuments(db , "dishes");             // Returned Promise
+        })
+
+        .then((docs) => {                                           // takes return from above
+            console.log('Found Documents:\n', docs);                // and carryon call 
+
+            return dboper.updateDocument(db , {"name": "Capacino"} ,
+                    { "description": "Updated Test" } , "dishes");
+        })
+
+        .then((result) => {
+            console.log('Updated Document:\n' , result.result);
+
+            return dboper.findDocuments(db , "dishes");
+        })
+
+        .then((docs) => {
+            console.log('Found Udated Documents:\n', docs);
+                        
+            return db.dropCollection("dishes");
+        })
+
+        .then((result) => {
+            console.log("Dropped Collection: " , result);
+
+            db.close();
+        })
+
+        .catch((err) => {console.log(err)})
+                       
+    } , (err) => { console.log(err) })
+        .catch((err) => {console.log(err)});    
+
     
-    //   *** Method-2 (Using a File Based Module) ***
+
+
+    /*   *** Method-2 (Using a File Based Module) ***
+    const collection = db.collection('dishes');
+
     dboper.insertDocument(db , { "name": "Capacino", "description": "Test"} , 
         "dishes" , (result) => {
             console.log('Inserted Documnet:\n' , result.ops);
@@ -37,11 +76,12 @@ MongoClient.connect(url , (err , db) => {
                     });
             });
         });
-
+    */    
 
 
 
     /*    *** Method - 1 (Normal    Without Creating a File based Module) ***
+    const collection = db.collection('dishes');
 
     collection.insertOne({"name": "Uthapizza", "description": "test"} ,
         (err , result) => {
@@ -64,4 +104,3 @@ MongoClient.connect(url , (err , db) => {
             })
         });
     */    
-});
--- operations.js
+++ operations.js
@@ -1,5 +1,30 @@
 const assert = require('assert');
 
+// *** Method-3 (Using a File Based Module - Included Promises to Solve Callback Hell) ***
+// Returned part is handled in index.js file 
+
+exports.insertDocument = (db , document , collection , callback) => {
+    const coll = db.collection(collection);
+    return coll.insert(document);  // callback removed and returned for promise
+};
+
+exports.findDocuments = (db , collection , callback) => {
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();  // callback removed and returned for promise
+};
+
+exports.removeDocument = (db , document , collection , callback) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);  // callback removed and returned for promise
+};
+
+exports.updateDocument = (db , document , update ,  collection , callback) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document , { $set: update} , null);  // callback removed and returned for promise
+};
+
+
+/*   *** Method-2 (Using a File Based Module) ***
 exports.insertDocument = (db , document , collection , callback) => {
     const coll = db.collection(collection);
     coll.insert(document , (err , result) => {
@@ -37,3 +62,4 @@ exports.updateDocument = (db , document , update ,  collection , callback) => {
         callback(result);
     });
 };
+*/
